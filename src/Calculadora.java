bd8bb1bf34d71717b25417000f97f05de1989b16
thur <arthur_ferrari@estudante.sesisenai.org.br>
2025-08-28 15:28:13 -0300

    Commit Divisão

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -2,4 +2,7 @@ public class Calculadora {
     public static int multiplique(int a, int b){
         return a * b;
     }
+    public static int divida(int a, int b){
+        return a / b;
+    }
 }
